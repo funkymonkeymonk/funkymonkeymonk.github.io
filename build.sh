87aac2f3fa45749a3c4c24b9e403f6e95e0a9732
funkymonkeymonk <monkey@buildingbananas.com>
2015-10-13 20:12:27 -0400

    Initial configuration

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function run_jekyll {
+  docker run -it --rm --volume=$(pwd):/srv/jekyll \
+    -p 4000:4000 jekyll/jekyll:pages jekyll s
+}
+
+# For development
+run_jekyll
+
+
+# I feel like there has to be a way to get grunt to work wit/inside
+# a docker container. I want to be able to make changes to the page, and have
+# all tests run in the container, while also having the change reflected in
+# my browser
